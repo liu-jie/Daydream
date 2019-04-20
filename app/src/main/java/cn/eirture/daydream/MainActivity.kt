5098b2f66e74e51299240ff938640ca795c1db13
Liu Jie <liujie@genedock.com>
2019-04-20 15:18:29 +0800

    Change size of clock text automatically

--- app/src/main/java/cn/eirture/daydream/MainActivity.kt
+++ app/src/main/java/cn/eirture/daydream/MainActivity.kt
@@ -4,12 +4,14 @@ import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.OrientationEventListener
+import android.widget.TextClock
 import java.util.*
 import java.util.function.Consumer
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mOrientationEventListener: OrientationEventListener
+    private lateinit var tvClock: TextClock
 
     private fun getOrientation(v: Int) = when (v) {
         in 0..30 -> ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
@@ -22,12 +24,24 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+        initViews()
+        initSensors()
+
+    }
+
+    private fun initViews() {
+        tvClock = findViewById(R.id.tvClock)
+        tvClock.textSize = 100f
+    }
+
+    private fun initSensors() {
         mOrientationEventListener = MyOrientationEventListener(this, Consumer {
             val newOrientation = getOrientation(it)
             if (newOrientation != null) {
                 requestedOrientation = newOrientation
+                tvClock.textSize =
+                    if (newOrientation == ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT) 100f else 160f
             }
         })
     }
